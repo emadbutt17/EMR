934f5bb13f17019a36cf9fd1d2158c377d5beee1
Colin Harrison <coharrison25@gmail.com>
2019-10-13 21:22:47 -0400

    added to patient search and added bash script for bundling

--- src/js/patient_search.js
+++ src/js/patient_search.js
@@ -1,34 +1,42 @@
 var PouchDB = require('pouchdb-browser');
+PouchDB.plugin(require('pouchdb-find'));
 var patientDb = new PouchDB('patients');
 
 // Create an index on patient name so we can search for them easily
 // Does nothing if index already exists
 patientDb.createIndex({
-  index: {
-      fields: ['name']
-  }
-}, function(err, result) {
-  if (err) {
-      return console.log(err);
-  }
-
-  // TODO: handle results
-  console.log(result);
+    index: {
+        fields: ['name']
+    }
+}, function (err, result) {
+    if (err) {
+        console.log(err);
+    }
 });
 
-function searchForPatientByName() {
-  let patientName = document.getElementById('searchByNameText').value;
+function getPatientsByName() {
+    let patientName = document.getElementById('searchByNameText').value;
 
-  patientDb.find({
-      selector: {name: patientName},
-      fields: ['_id', 'name'],
-      sort: ['name']
-  }, function(err, result) {
-      if (err) { console.log(err); }
-      // handle result
-      console.log('we did it: ')
-      console.log(result);
-  });
+    patientDb.find({
+        selector: { name: patientName },
+        fields: ['_id', 'name'],
+        sort: ['name']
+    }, function (err, result) {
+        if (err) {
+            console.log(err);
+            return null;
+        }
+
+        console.log(result);
+        return result;
+    });
+}
+
+function searchForPatientByName() {
+    let patients = getPatientsByName();
+    if (patients != null) {
+        // display the patients on the page and reformat all the html
+    }
 };
 
 var searchByNameButton = document.getElementById('searchByNameButton');
