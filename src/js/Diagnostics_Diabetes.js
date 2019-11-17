1f2d7703f36ddef90229689c8c1451aabec5531c
Colin Harrison <coharrison25@gmail.com>
2019-11-17 14:52:25 -0500

    checkpoint

--- src/js/Diagnostics_Diabetes.js
+++ src/js/Diagnostics_Diabetes.js
@@ -2,29 +2,50 @@ const uuidv4 = require('uuid/v4');
 var PouchDB = require('pouchdb-browser');
 var patientDb = new PouchDB('patients');
 
-diabetesButton = document.getElementById('diabetesButton');
+let diabetesButton = document.getElementById('diabetesButton');
 diabetesButton.onclick = addDiabetes;
 
-function addDiabetes(patient){
-    vision = document.getElementById('vision').value;
-    bloodTop = document.getElementById('bloodTop').value;
-    bloodBottom = document.getElementById('bloodBottom').value;
-    bloodSugar = document.getElementById('bloodSugar').value;
-    hemoglobin = document.getElementById('hemoglobin').value;
-    insulin = document.getElementById('insulin').value;
-    ifYes = document.getElementById('ifYes').value;
-    
-    let diabetes = {
-        _id: uuidv4(),
-        type: 'diabetes',
-        vision: vision,
-        bloodTop: bloodTop,
-        bloodBottom: bloodBottom,
-        bloodSugar: bloodSugar,
-        hemoglobin: hemoglobin,
-        insulin: insulin,
-        ifYes: ifYes
-    };
-    patient.diagnoses.append(diabetes);
-    patientDb.put(patient);
+let queryParams = document.location.search;
+const id = queryParams.substring(queryParams.indexOf("=") + 1);
+
+function addDiabetes(){
+    patientDb.get(id)
+        .then((patient) => {
+            diet = document.getElementById('diet').value;
+            vision = document.getElementById('vision').value;
+            bloodTop = document.getElementById('bloodTop').value;
+            bloodBottom = document.getElementById('bloodBottom').value;
+            bloodSugar = document.getElementById('bloodSugar').value;
+            hemoglobin = document.getElementById('hemoglobin').value;
+            insulin = document.getElementById('insulin').value;
+            ifYes = document.getElementById('ifYes').value;
+            
+            let diabetes = {
+                _id: uuidv4(),
+                type: 'diabetes',
+                diet: diet,
+                vision: vision,
+                bloodTop: bloodTop,
+                bloodBottom: bloodBottom,
+                bloodSugar: bloodSugar,
+                hemoglobin: hemoglobin,
+                insulin: insulin,
+                ifYes: ifYes,
+                date: new Date().toLocaleDateString('en-GB')
+            };
+
+            if (patient.diagnoses) {
+                patient.diagnoses.push(diabetes);
+            } else {
+                patient.diagnoses = [diabetes];
+            }
+            
+            patientDb.put(patient)
+                .then((res) => {
+                    document.location.href = './patient_page.html?id=' + id;
+                })
+                .catch((err) => {
+                    console.log(err);
+                });
+        });
 }
--- src/js/patient_page.js
+++ src/js/patient_page.js
@@ -3,13 +3,41 @@ var patientDb = new PouchDB('patients');
 
 // On Load
 let header = document.getElementById('page-header');
+let asthmaButton = document.getElementById('asthma');
+let depressionButton = document.getElementById('depression');
+let diabetesButton = document.getElementById('diabetes');
+let pregnancyButton = document.getElementById('pregnancy');
+let epilepsyButton = document.getElementById('epilepsy');
+let hypertensionButton = document.getElementById('hypertension');
 
 let queryParams = document.location.search;
 const id = queryParams.substring(queryParams.indexOf("=") + 1);
 
+asthmaButton.onclick = () => {
+    document.location.href = './diagnostics_asthma.html?id=' + id;
+}
+depressionButton.onclick = () => {
+    document.location.href = './diagnostics_depression.html?id=' + id;
+}
+diabetesButton.onclick = () => {
+    document.location.href = './Diagnostics_Diabetes.html?id=' + id;
+}
+pregnancyButton.onclick = () => {
+    document.location.href = './Diagnostics_Pregnancy.html?id=' + id;
+}
+epilepsyButton.onclick = () => {
+    document.location.href = './Diagnostics_Epilepsy.html?id=' + id;
+}
+hypertensionButton.onclick = () => {
+    document.location.href = './Diagnostics_Hypertension.html?id=' + id;
+}
+
 patientDb.get(id)
 .then(function(doc) {
     header.innerText = doc.name;
+    document.getElementById('patient-sex').innerText = 'Sexo: ' + doc.sex;
+    document.getElementById('patient-weight').innerText = 'Peso: ' + doc.weight + ' kg';
+    document.getElementById('patient-height').innerText = 'Altura: ' + doc.height + ' cm';
 })
 .catch(function(err) {
     console.log(err);
@@ -18,12 +46,7 @@ patientDb.get(id)
 // Button Actions
 let patientHistoryButton = document.getElementById('patient-history-button');
 patientHistoryButton.addEventListener("click", function() {
-    document.location.href = "./patient_history.html?id=" + id;
-});
-
-let patientDiagnosesButton = document.getElementById('patient-diagnoses-button');
-patientDiagnosesButton.addEventListener('click', function() {
-    document.location.href = './patient_diagnoses_flex.html?id=' + id;
+    document.location.href = "./patient_diagnoses_flex.html?id=" + id;
 });
 
 let patientPrescriptionsButton = document.getElementById('patient-prescriptions-button');
@@ -33,5 +56,43 @@ patientPrescriptionsButton.addEventListener('click', function() {
 
 let patientCheckupsButton = document.getElementById('new-checkup-button');
 patientCheckupsButton.addEventListener('click', function() {
-    document.location.href = './Diagnostics_Notes.html?id=' + id;
+
+    patientDb.get(id)
+        .then(function(patient) {
+            let dob = patient.dob;
+            let sex = patient.sex;
+            let years = diff_years(new Date(), new Date(dob));
+            if (years < 1) {
+                document.location.href = './Evaluations_Newborn.html?id=' + id;
+            } else if (years > 0 && years < 5) {
+                document.location.href = './Evaluations_Children_5.html?id=' + id;
+            } else if (years > 4 && years < 10) {
+                document.location.href = './Evaluations_Children_9.html?id=' + id;
+            } else if (years > 9 && years < 20) {
+                document.location.href = './Evaluations_Children_19.html?id=' + id;
+            } else if (years > 19 && years < 60) {
+                if (sex === 'M' || sex === 'm' || sex === 'Male' || sex === 'male') {
+                    document.location.href = './Evaluations_Men_20-59.html?id=' + id;
+                } else {
+                    document.location.href = './Evaluations_Women_20-59.html?id=' + id;
+                }
+            } else if (years > 59) {
+                if (sex === 'M' || sex === 'm' || sex === 'Male' || sex === 'male') {
+                    document.location.href = './Evaluations_Men_60.html?id=' + id;
+                } else {
+                    document.location.href = './Evaluations_Women_60.html?id=' + id;
+                }
+            }
+        })
+        .catch(function(err) {
+            console.log(err);
+        });
 });
+
+let diff_years = (dt2, dt1) => {
+
+    var diff =(dt2.getTime() - dt1.getTime()) / 1000;
+    diff /= (60 * 60 * 24);
+    return Math.abs(Math.floor(diff/365.25));
+   
+}
